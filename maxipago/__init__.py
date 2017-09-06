716ff1ab16ae4d0c57f480abb4447b51649d4fa0
Fernando Paiva <fernandosjp@gmail.com>
2017-09-05 21:11:13 -0300

    [Py3 compatibility] Futurize stage 2 applied

--- maxipago/__init__.py
+++ maxipago/__init__.py
@@ -1,7 +1,14 @@
 """maxiPago python integration"""
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
 # :copyright: (c) 2013 by Stored (www.stored.com.br).
 # :license:   BSD, see LICENSE for more details.
 
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
 __version__ = '1.0.1'
 __author__ = 'Stored'
 __contact__ = 'contato@stored.com.br'
--- maxipago/client.py
+++ maxipago/client.py
@@ -1,6 +1,14 @@
 # coding: utf-8
 
 
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
+from builtins import object
 class Maxipago(object):
 
     def __init__(self, maxid, api_key, api_version='3.1.1.15', sandbox=False):
